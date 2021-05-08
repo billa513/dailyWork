29471935cfda9d7929d0d93c5bf69681f1fda158
billa513 <72115147+billa513@users.noreply.github.com>
2021-05-08 18:09:52 +0530

    Add files via upload

---
+++ jQuery/Js/index.js
@@ -0,0 +1,28 @@
+
+    // $(document).ready(function(){
+    // $('p').addClass('add-color');
+    // });
+
+    //  $(function(){
+    //      $('#hide').click(()=>{
+    //         $('p').hide();
+    //      });
+    //      $('#show').click(()=>{
+    //         $('p').show();
+    //      });
+    //     });
+//on clicking change color
+        // $(function(){
+        //     $('button').click(()=>{
+        //        $('p').addClass('add-color');
+        //     });
+        //    });
+//fast method
+        // $(()=>{
+        //     $('p').addClass('add-color');
+        //     });
+        $(()=>{
+             $('button').click(()=>{
+                 $('p').toggle();
+             });
+                });
---
+++ jQuery/dailyWork/README.md
@@ -0,0 +1,2 @@
+# dailyWork
+dailytasks
