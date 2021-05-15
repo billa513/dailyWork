672b483e4f935e94fa64406363feccecfcc72a93
billa513 <billamahesh12345@gmail.com>
2021-05-15 11:34:07 +0530

    work

--- Jquery/js/index.js
+++ Jquery/js/index.js
@@ -1,19 +1,44 @@
-//$(document).ready(function() {
-//	$('p').addClass('add-color');
-// });
 
-$(()=> {
-    $('button').click(()=> {
-        $('p').addClass('add-color');
-    });
-    $('#hide').click(()=> {
-        $('p').hide();
-    });
-    $('#show').click(()=> {
-        $('p').show();
-    });
-    $('#toggle').click(()=> {
-        $('p').toggle();
-    });
-    
-});
+    // $(document).ready(function(){
+    // $('p').addClass('add-color');
+    // });
+
+    //  $(function(){
+    //      $('#hide').click(()=>{
+    //         $('p').hide();
+    //      });
+    //      $('#show').click(()=>{
+    //         $('p').show();
+    //      });
+    //     });
+//on clicking change color
+        // $(function(){
+        //     $('button').click(()=>{
+        //        $('p').addClass('add-color');
+        //     });
+        //    });
+//fast method
+        // $(()=>{
+        //     $('p').addClass('add-color');
+        //     });
+        // $(()=>{
+        //      $('button').click(()=>{
+        //          $('p').toggle();
+        //      });
+        //         });
+       // $(()=>$('div').addClass('add-color'));
+        $(() => {
+            $('#selected-plays > li').addClass('horizontal');
+            $('#selected-plays li:not(.horizontal)').addClass('sub-level');
+            $('#selected-plays li:not(.horizontal):even').addClass('alt');
+            
+        });
+        $(() => {
+            $('a[href^="mailto:"]').addClass('mailto');
+            $('a[href$=".pdf"]').addClass('pdflink');
+            $('a[href^="http"][href*="henry"]') .addClass('henrylink');
+        });
+        $(() => { 
+            $('tr:even').addClass('alt'); 
+            $('td:contains(Henry)') .addClass('highlight'); 
+           }); 
