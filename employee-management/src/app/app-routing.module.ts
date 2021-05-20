3ae4af7c3cbee59bd85f02e378794aed62e51eb7
billa513 <72115147+billa513@users.noreply.github.com>
2021-05-20 10:39:11 +0530

    Add files via upload

---
+++ employee-management/src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { StudentComponent } from './student/student.component';
+
+import { UpdateEmployeeComponent } from './employee/update-employee/update-employee.component';
+import { EMPLOYEES } from './employee-mock';
+import { EmployeeComponent } from './employee/employee.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { AddEmployeeComponent } from './employee/add-employee/add-employee.component';
+
+// const routes:Routes=[
+//   {path:'',component:EmployeeComponent},
+//   {path:'add-employee',component:AddEmployeeComponent},
+//   {path:'employee/:id',component:EmployeeComponent},
+//   {path:'update-employee',component:UpdateEmployeeComponent}
+// ];
+const routes:Routes=[
+  {path:'',component:StudentComponent}
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,RouterModule.forRoot(routes)
+  ],
+  exports:[RouterModule]
+})
+export class AppRoutingModule { }
